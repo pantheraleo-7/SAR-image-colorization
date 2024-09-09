e9a5a5892beca476ceb99dd28448190ff58f91cb
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2024-09-09 12:42:42 +0530

    fix last batch error; speed up device transfer

--- train.py
+++ train.py
@@ -15,9 +15,9 @@ data = SAROpticalDataset(root_dir='~/Downloads/dataset', transform=transform)
 
 train_dataset, valid_dataset = random_split(data, [0.8, 0.2])
 
-BATCH_SIZE = 16
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False)
+BATCH_SIZE = 32
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True, drop_last=True)
+valid_loader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False, pin_memory=True, drop_last=True)
 
 device = torch.device('cuda' if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else 'cpu'))
 print(device)
@@ -39,9 +39,9 @@ for epoch in range(1, num_epochs+1):
 
     train_loss = 0.0
     for i, (sar_img, opt_img) in enumerate(train_loader, start=1):
-        real = torch.ones(BATCH_SIZE, 1, 13, 13).to(device)
-        fake = torch.zeros(BATCH_SIZE, 1, 13, 13).to(device)
         sar_img, opt_img = sar_img.to(device), opt_img.to(device)
+        fake = torch.zeros(BATCH_SIZE, 1, 13, 13).to(device)
+        real = torch.ones(BATCH_SIZE, 1, 13, 13).to(device)
 
         # Train Generator
         optimizer_g.zero_grad()
@@ -61,14 +61,14 @@ for epoch in range(1, num_epochs+1):
 
         train_loss += (loss_g.item()+loss_d.item())*BATCH_SIZE
 
-        if i%10==0: print(f'Batch {i}/{len(train_loader)} | G loss: {loss_g:.4f} | D loss: {loss_d:.4f}')
+        if i%100==0: print(f'Batch {i}/{len(train_loader)} | G loss: {loss_g:.4f} | D loss: {loss_d:.4f}')
 
     train_loss /= len(train_dataset)
 
     generator.eval()
 
-    valid_loss = 0.0
     with torch.no_grad():
+        valid_loss = 0.0
         for sar_img, opt_img in valid_loader:
             sar_img, opt_img = sar_img.to(device), opt_img.to(device)
             color_img = generator(sar_img)
