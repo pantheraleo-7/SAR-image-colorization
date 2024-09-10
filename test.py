2d7feaeb64545350d8e486ac9bbb795b58074129
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2024-09-10 21:02:51 +0530

    add mse & batch visualization to test.py

--- test.py
+++ test.py
@@ -1,22 +1,23 @@
-import matplotlib.pyplot as plt
-from torchvision import io
+import torch
+from torch.utils.data import DataLoader, Subset
+from torchvision import io, utils
 
+from dataset import SAROpticalDataset
 from output import colorize
 
-test_img = io.read_image('random.png', io.ImageReadMode.GRAY)
-pred_img = colorize(test_img.unsqueeze(0)).squeeze()
+dataset = SAROpticalDataset(root_dir='~/Downloads/dataset')
 
-# Visualize the result
-plt.figure(figsize=(10, 5))
+test_size = 10
+test_data = Subset(dataset, range(1, len(dataset), len(dataset)//test_size))
 
-plt.subplot(1, 2, 1)
-plt.imshow(test_img.permute(1, 2, 0).numpy(), cmap='gray')
-plt.title('Input SAR Image (grayscale)')
-plt.axis('off')
+test_loader = DataLoader(test_data, batch_size=test_size, shuffle=False, drop_last=True)
 
-plt.subplot(1, 2, 2)
-plt.imshow(pred_img.permute(1, 2, 0).numpy())
-plt.title('Output Optical Image (RGB)')
-plt.axis('off')
+for sar_imgs, opt_imgs in test_loader:
+    color_imgs = colorize(sar_imgs)
 
-plt.show()
+    mse = torch.mean(torch.pow(opt_imgs-color_imgs, 2), dtype=torch.float32)
+    print('Mean Squared Error:', mse.item())
+
+    imgs = [img for pair in zip(opt_imgs, color_imgs) for img in pair]
+    nrow = half if (half:=test_size//2)%2==0 else half-1
+    io.write_png(utils.make_grid(imgs, nrow), 'test_grid.png')
