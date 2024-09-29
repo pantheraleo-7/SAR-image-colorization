90381b279ecb5f86ec0d334f312feed029bc07d4
Asadullah Shaikh <asadshaikh20022002@gmail.com>
2024-09-29 18:40:41 +0530

    fmt

--- frontend/src/components/colorizedImage.jsx
+++ frontend/src/components/colorizedImage.jsx
@@ -1,16 +1,18 @@
 import ImageComparison from "./slider";
 function ImageComparisonArray(props) {
-    const { image } = props
-    return (
-        <>
-            {
-                image.map((img, index) => (
-                    <ImageComparison src1={img} src2={props.ColorizedImage[index]} key={index} position={40} flag={true} />))
-            }
-            
-        </>
-
-
-    )
+  const { image } = props;
+  return (
+    <>
+      {image.map((img, index) => (
+        <ImageComparison
+          src1={img}
+          src2={props.ColorizedImage[index]}
+          key={index}
+          position={40}
+          flag={true}
+        />
+      ))}
+    </>
+  );
 }
 export default ImageComparisonArray;
